4f030dd18084aeb8584b0646f19c2d8270145519
maysa92 <appleswill29@gmail.com>
2020-09-09 20:09:23 -0500

    clean up

--- src/Trivium.java
+++ src/Trivium.java
@@ -6,17 +6,16 @@ public class Trivium {
 	String keystream_b;
     String plaintext = "";
 	String newplaintext;
-	
     int[] A_IV = new int[93];
     int[] Bkey = new int[84];
     int[] arrayC = new int[111];
-    
     int m;
     int c1, c2, c3, c4, c5, c6;
-    int keystream;
+	int keystream;
+	String input;
+	int[] result = new int [3000];
     
     {
-    
     //initialize A, B, C
     for (int i = 0; i < arrayC.length; i++) {
     	if (i == 108) {
@@ -46,33 +45,30 @@ public class Trivium {
     	}
     }
     
-    String input;
+
     //ask for plain text
-    public String userinput() {
-    Scanner userinput = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter the plain text: ");
-    this.input = userinput.nextLine(); 
-    return input;
+    String userinput() {
+    	Scanner userinput = new Scanner(System.in);  // Create a Scanner object
+    	System.out.println("Enter the plain text: ");
+    	this.input = userinput.nextLine(); 
+    	return input;
     }
     
-    int[] result = new int [3000];
-    
+
     //convert plain text to binary   
-    public void stringtoBinary(String string){
+    void stringtoBinary(String string){
         String binary = "";
 		int x;
         char[] Char = string.toCharArray();
         for (int i = 0; i < Char.length; i++) {
             String tmp;
         	tmp = Integer.toBinaryString(Char[i]);
-            
             x = tmp.length();
             if (x != 8){
                 x = 8-x;
                 if (x == 8){
                     binary += tmp;
-                } 
-                else if (x > 0 ){
+                } else if (x > 0 ){
                     for (int j = 0; j < x; j++) {
                         binary += "0"; 
                     }
@@ -80,43 +76,36 @@ public class Trivium {
             }            
             binary += tmp;
         } 
-
         this.result = Arrays.stream(binary.split("")).mapToInt(Integer::parseInt).toArray();
       }
     
     
     //right shift
-    public void helper() {
+    void helper() {
     	for (int i = A_IV.length - 1; i >= 0; i--) {
-    	    if(i == 0) {
-    	    	A_IV[0] = c3;
-    	    }
+    	    if(i == 0) A_IV[0] = c3;
     	    else {
     	    	A_IV[i] = A_IV[i-1];
     	    }
     	}
     	for (int i = Bkey.length - 1; i >=0; i--) {
-    	    if(i == 0) {
-    	    	Bkey[0] = c1;
-    	    }
+    	    if(i == 0) Bkey[0] = c1;
     	    else {
     	    	Bkey[i] = Bkey[i-1];
     	    }
     	}
     	for (int i = arrayC.length - 1; i >=0; i--) {
-    	    if(i == 0) {
-    	    	arrayC[0] = c2;
-    	    }
-    	    else {arrayC[i] = arrayC[i-1];}
+    	    if(i == 0) arrayC[0] = c2;
+    	    else{
+				arrayC[i] = arrayC[i-1];
+			}
     	}
-    	/*for(int i= 0; i < A_IV.length; i++) {
-    	System.out.println(A_IV[i]);}*/	
     }
     
     
     
     //algorithm
-    public int generateKey() {
+    int generateKey() {
     	c1 = (A_IV[65] ^ A_IV[90] & A_IV[91] ^ A_IV[92] ^ Bkey[77]);
     	c2 = (Bkey[68] ^ Bkey[81] & Bkey[82] ^ Bkey[83] ^arrayC[87]);
     	c3 = (arrayC[65]^arrayC[108]& arrayC[109]^arrayC[110]^A_IV[68]);
@@ -128,25 +117,23 @@ public class Trivium {
     	return keystream;
     }
     
-    
     //the warm up
     int[] keystreamoutput = new int [3000];
-    public void warmup() {
-    	for ( int i = 0; i < 1153 ; i++) {
+    void warmup() {
+    	for (int i = 0; i < 1153 ; i++) {
     		generateKey();
     		helper();
     	}
-    	for ( int i = 1153 ; i <result.length+1153 ; i++) {
+    	for (int i = 1153 ; i <result.length+1153 ; i++) {
     		int tmp = generateKey();
     		helper();
     		keystreamoutput [i -1153] = tmp;
     	}
-    	
     }
     
     //encryption
     int[] cypher = new int[3000];
-    public void encrypt(){
+    void encrypt(){
     	System.out.println("Ciphers: ");
     	stringtoBinary(input);
     	for (int i = 0; i < result.length; i++) {
@@ -158,7 +145,7 @@ public class Trivium {
     }
     
     //decryption
-    public void decrypt(){
+    void decrypt(){
     	int[] decryption = new int[3000];	
     	System.out.println("Plaintext Decrypted: ");
     	for (int i = 0; i < cypher.length; i++) {
@@ -179,10 +166,6 @@ public class Trivium {
         tr.userinput();
         long startTime = System.nanoTime();
         tr.warmup();
-        /*System.out.println("Keystream: ");
-        for (int y = 0; y < 1000; y++) {
-        	System.out.print(tr.keystreamoutput[y]);
-        }*/
         System.out.println();
         tr.encrypt(); 
         long elapsedTime = System.nanoTime() - startTime;
